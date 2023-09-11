3037bd79155db3a6fec3231f09c48ba07d51a69d
Suyeong Yang <ysw1mst@naver.com>
2023-09-11 15:17:06 +0900

    자바 예제 업데이트 테스트

---
+++ Day_01_Practice/VarEx1.java
@@ -0,0 +1,19 @@
+package Day_01_Practice;
+
+import java.sql.SQLOutput;
+
+public class VarEx1 {
+    public static void main(String[] args) {
+        int year = 0;
+        int age = 14;
+
+        System.out.println(year);
+        System.out.println(age);
+
+        year = age + 2000;
+        age = age + 1;
+
+        System.out.println(year);
+        System.out.println(age);
+    }
+}
